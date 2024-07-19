654171d5781e9d587bea3f4f759f60f2ff89d90d
Maurdekye <ncolaprete@gmail.com>
2024-07-20 00:54:58 +0300

    added basic doctest, toml details

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,36 @@
 //! Implement `Eq + Hash + Clone` for your search space state type to benefit from prior explored state checking optimization; if youre unable to, then use the `SearchUnhashable` or `ScoredSearchUnhashable` iterators, which do not require these additional bounds, but will likely explore the space much less efficiently.
 //!
 //! When implementing `ScoredSearch`, make sure that higher scoring states are closer to a solution.
+//!
+//! ```
+//! use space_search::*;
+//! use std::{vec, hash::Hash};
+//!
+//! #[derive(Clone, Debug, PartialEq, Eq, Hash)]
+//! struct Pos(i32, i32);
+//!
+//! impl Searchable for Pos {
+//!     type NextMoveIterator = vec::IntoIter<Pos>;
+//!
+//!     fn next_states(&self) -> Self::NextMoveIterator {
+//!         let &Pos(x, y) = self;
+//!         vec![
+//!             Pos(x - 1, y),
+//!             Pos(x, y - 1),
+//!             Pos(x + 1, y),
+//!             Pos(x, y + 1),
+//!         ].into_iter()
+//!     }
+//!
+//!     fn is_solution(&self) -> bool {
+//!         let &Pos(x, y) = self;
+//!         x == 5 && y == 5
+//!     }
+//! }
+//!
+//! let mut searcher = Search::new(Pos(0, 0));
+//! assert_eq!(searcher.next(), Some(Pos(5, 5)));
+//! ```
 
 use std::{
     collections::{BinaryHeap, HashSet, VecDeque},
